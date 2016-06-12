4097a90b1af08fc6735518b023ffe3c4f413f860
VitBuk <vitbuk@gmail.com>
2016-06-12 19:51:14 +0300

    start

---
+++ Main.py
@@ -0,0 +1,17 @@
+# p - players
+# q - questions
+
+# for test
+p_count = 30
+q_count = 36
+q_in_round = 6
+
+# constants
+MAX_TABLE_CAPACITY = 6
+
+
+def route(p_count, q_count, q_in_round):
+    return 0
+
+
+
