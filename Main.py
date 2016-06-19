4697b46c84469047ca3582e947c38658d8179c27
VitBuk <vitbuk@gmail.com>
2016-06-19 21:27:01 +0300

    save in files

--- Main.py
+++ Main.py
@@ -13,5 +13,10 @@ MAX_TABLE_CAPACITY = 6
 def route(p_count, q_count, q_in_round):
     return 0
 
+fobj = open("C:/Users/VITAL/Desktop/Mill/ad_leasbiam.txt")
+for line in fobj:
+    print(line.rstrip())
+fobj.close()
+
 
 
